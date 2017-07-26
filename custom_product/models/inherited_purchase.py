16dd2ec85e11baebc87521d60963ec871ef788d2
susai <susai@susai-Aspire-4739Z>
2017-07-26 23:24:59 +0530

    Changed product master updation.

--- custom_product/models/inherited_purchase.py
+++ custom_product/models/inherited_purchase.py
@@ -24,7 +24,7 @@ class PurchaseOrderLine(models.Model):
 
         # Reset date, price and quantity since _onchange_quantity will provide default values
         self.date_planned = datetime.today().strftime(DEFAULT_SERVER_DATETIME_FORMAT)
-        self.price_unit = self.product_id.list_price
+        self.price_unit = self.product_id.product_tmpl_id.standard_price
         self.income_percentage = self.product_id.income_percentage
         self.sale_price = self.product_id.list_price
         self.product_uom = self.product_id.uom_po_id or self.product_id.uom_id
@@ -50,6 +50,18 @@ class PurchaseOrderLine(models.Model):
         self._onchange_quantity()
         return result
     
+    
+    
+#     if self.standard_price and self.vat_id and not self.income_percentage:
+#             self.list_price = (self.standard_price * (self.vat_id.amount/100)) + self.standard_price
+#             self.vat_amt = self.list_price - self.standard_price
+#         if self.standard_price and self.vat_id and self.income_percentage:
+#             actual_vat_amt = (self.standard_price * (self.vat_id.amount/100)) + self.standard_price
+#             self.vat_amt = actual_vat_amt - self.standard_price
+#             self.income_amt = (self.standard_price + self.vat_amt) * (self.income_percentage/100)
+#             self.list_price = self.vat_amt + self.standard_price + self.income_amt
+#     
+    
     @api.depends('product_qty', 'price_unit', 'taxes_id','income_percentage')
     def _compute_amount(self):
         for line in self:
@@ -57,20 +69,31 @@ class PurchaseOrderLine(models.Model):
             tax_amt = 0.0;
             for tax_id in line.taxes_id:
                 tax_amt += tax_id.amount
-            if line.income_percentage and line.taxes_id:
-                pp = (line.price_unit*tax_amt/100)+line.price_unit
-                income = (pp*line.income_percentage/100)
-                line.sale_price = income +pp
+            if line.price_unit and line.taxes_id and not line.income_percentage:
+                line.sale_price = (line.price_unit * (tax_amt/100)) + line.price_unit
+            if line.price_unit and line.taxes_id and line.income_percentage:
+                actual_vat_amt = (line.price_unit * (tax_amt/100))
+                income_amt = (line.price_unit + actual_vat_amt) * (line.income_percentage/100)
+                line.sale_price = actual_vat_amt + line.price_unit + income_amt
+#             if line.income_percentage and line.taxes_id:
+#                 pp = (line.price_unit*tax_amt/100)+line.price_unit
+#                 income = (pp*line.income_percentage/100)
+#                 line.sale_price = income +pp
             line.update({
                 'price_tax': taxes['total_included'] - taxes['total_excluded'],
                 'price_total': taxes['total_included'],
                 'price_subtotal': taxes['total_excluded'],
             })
             #product price update
-            for tax_id in line.taxes_id:
-                line.product_id.write({'vat_id':tax_id.id,'income_percentage':line.income_percentage})
+#             line.product_id.product_tmpl_id.write({'standard_price':line.price_unit})
+#             line.product_id.product_tmpl_id.standard_price = line.price_unit
+#             for tax_id in line.taxes_id:
+#                 line.product_id.write({'vat_id':tax_id.id,'income_percentage':line.income_percentage})
+#                 line.product_id.standard_price = line.price_unit
+#                 print line.product_id.standard_price
+#                 line.product_id.update({'standard_price':line.price_unit})
         return True
-            
-            
+#             
+#             
             
             
