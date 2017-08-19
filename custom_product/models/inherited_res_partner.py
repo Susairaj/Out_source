119f6e4b8c817eb631bb28804a3086204e25e106
Susairaj <susaimaths@gmail.com>
2017-08-19 08:50:34 +0530

    Updated vat updation method

--- custom_product/models/inherited_res_partner.py
+++ custom_product/models/inherited_res_partner.py
@@ -5,3 +5,15 @@ class ResPartner(models.Model):
     _inherit = 'res.partner'
 
     tin_no = fields.Char ('GST TIN')
+    
+class UpdateProduct(models.Model):
+    _name = 'update.product'
+
+    name = fields.Char ('Name')
+    
+    @api.multi
+    def update_vat(self):
+        product_ids = self.env['product.product'].search([])
+        for product_id in product_ids:
+            if not product_id.vat_percentage:
+                product_id.vat_percentage = product_id.vat_id.amount
